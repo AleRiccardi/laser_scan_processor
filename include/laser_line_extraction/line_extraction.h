29220d3bf58fa3f810f61abc70496d9169d5d141
Alessandro <alericcardi1996@gmail.com>
2020-04-17 16:29:19 +0200

    Init project

---
+++ include/laser_line_extraction/line_extraction.h
@@ -0,0 +1,61 @@
+#ifndef LINE_EXTRACTION_H
+#define LINE_EXTRACTION_H
+
+#include <cmath>
+#include <vector>
+#include <boost/array.hpp>
+#include <Eigen/Dense>
+#include "utilities.h"
+#include "line.h"
+
+namespace line_extraction
+{
+
+class LineExtraction
+{
+
+public:
+  // Constructor / destructor
+  LineExtraction();
+  ~LineExtraction();
+  // Run
+  void extractLines(std::vector<Line>&);
+  // Data setting
+  void setCachedData(const std::vector<double>&, const std::vector<double>&,
+                     const std::vector<double>&, const std::vector<unsigned int>&);
+  void setRangeData(const std::vector<double>&);
+  // Parameter setting
+  void setBearingVariance(double);
+  void setRangeVariance(double);
+  void setLeastSqAngleThresh(double);
+  void setLeastSqRadiusThresh(double);
+  void setMaxLineGap(double);
+  void setMinLineLength(double);
+  void setMinLinePoints(unsigned int);
+  void setMinRange(double);
+  void setMinSplitDist(double);
+  void setOutlierDist(double);
+
+private:
+  // Data structures
+  CachedData c_data_;
+  RangeData r_data_;
+  Params params_;
+  // Indices after filtering
+  std::vector<unsigned int> filtered_indices_;
+  // Line data
+  std::vector<Line> lines_;
+  // Methods
+  double chiSquared(const Eigen::Vector2d&, const Eigen::Matrix2d&,
+                    const Eigen::Matrix2d&);
+  double distBetweenPoints(unsigned int index_1, unsigned int index_2);
+  void   filterClosePoints();
+  void   filterOutlierPoints();
+  void   filterLines();
+  void   mergeLines();
+  void   split(const std::vector<unsigned int>&);
+};
+
+} // namespace line_extraction
+
+#endif
---
+++ include/laser_line_extraction/line_extraction_ros.h
@@ -0,0 +1,52 @@
+#ifndef LINE_EXTRACTION_ROS_H
+#define LINE_EXTRACTION_ROS_H
+
+#include <vector>
+#include <string>
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <visualization_msgs/Marker.h>
+#include <geometry_msgs/Point.h>
+#include "laser_scan_processor/LineSegment.h"
+#include "laser_scan_processor/LineSegmentList.h"
+#include "line_extraction.h"
+#include "line.h"
+
+namespace line_extraction
+{
+
+class LineExtractionROS
+{
+
+public:
+  // Constructor / destructor
+  LineExtractionROS(ros::NodeHandle&, ros::NodeHandle&);
+  ~LineExtractionROS();
+  // Running
+  void run();
+
+private:
+  // ROS
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh_local_;
+  ros::Subscriber scan_subscriber_;
+  ros::Publisher line_publisher_;
+  ros::Publisher marker_publisher_;
+  // Parameters
+  std::string frame_id_;
+  std::string scan_topic_;
+  bool pub_markers_;
+  // Line extraction
+  LineExtraction line_extraction_;
+  bool data_cached_; // true after first scan used to cache data
+  // Members
+  void loadParameters();
+  void populateLineSegListMsg(const std::vector<Line>&, laser_scan_processor::LineSegmentList&);
+  void populateMarkerMsg(const std::vector<Line>&, visualization_msgs::Marker&);
+  void cacheData(const sensor_msgs::LaserScan::ConstPtr&);
+  void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr&);
+};
+
+} // namespace line_extraction
+
+#endif
